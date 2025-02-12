0a19eac61a8b004774957293a75092048e4996c5
NuMeRo-999 <pedrovp003@gmail.com>
2025-02-12 01:35:27 +0100

    kinetic controller asset and movement rework

---
+++ Programación/3D-FPS/Assets/MovementRework/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private PlayerCharacter playerCharacter;
+    [SerializeField] private PlayerCamera playerCamera;
+
+    private PlayerInputActions.PlayerInputActions _inputActions;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        _inputActions = new PlayerInputActions.PlayerInputActions();
+        _inputActions.Enable();
+
+        playerCharacter.Initialize();
+        playerCamera.Initialize(playerCharacter.GetCameraTarget());
+    }
+
+    void OnDestroy()
+    {
+        _inputActions.Dispose();
+    }
+
+    void Update()
+    {
+        var input = _inputActions.Player;
+
+        var cameraInput = new CameraInput{ Look = input.Look.ReadValue<Vector2>() };
+        playerCamera.UpdateRotation(cameraInput);
+ 
+        playerCamera.UpdatePosition(playerCharacter.GetCameraTarget());
+
+        var characterInput = new CharacterInput
+        { 
+            Rotation = playerCamera.transform.rotation,
+            Move = input.Move.ReadValue<Vector2>(),
+            Jump = input.Jump.WasPressedThisFrame()
+        };
+        playerCharacter.UpdateInput(characterInput);
+    }
+}
